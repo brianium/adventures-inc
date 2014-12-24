2bd676ec8a79d17de63c7f10baa39c849b82fadf
Brian Scaturro <scaturrob@gmail.com>
2014-12-23 19:21:46 -0500

    exercise 2 complete

---
+++ exercises/2/ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    puts("Hello world.");
+    puts("It is so wonderful to see you");
+    puts("I am glad to be a part of this world");
+    puts("huzzah!");
+
+    return 0;
+}
