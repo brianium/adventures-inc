a35ee5981f0d9141a8a5d95ccd3f92a776b6cb3c
Brian Scaturro <scaturrob@gmail.com>
2014-12-26 11:45:24 -0500

    exercise 13 extra credit

--- exercises/13-switch-statement/ex13.c
+++ exercises/13-switch-statement/ex13.c
@@ -7,43 +7,41 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    int i = 0;
-    for (i = 0; argv[1][i] != '\0'; i++) {
-        char letter = argv[1][i];
+    int i;
+    char letter;
+    for (i = 0; letter = argv[1][i], argv[1][i] != '\0'; i++) {
+        letter = argv[1][i];
+        if (letter >= 'A' && letter <= 'Z') {
+            letter = letter + 'a' - 'A';
+        }
 
         switch(letter) {
             case 'a':
-            case 'A':
                 printf("%d: 'A'\n", i);
                 break;
 
             case 'e':
-            case 'E':
                 printf("%d: 'E'\n", i);
                 break;
 
             case 'i':
-            case 'I':
                 printf("%d: 'I'\n", i);
                 break;
 
             case 'o':
-            case 'O':
                 printf("%d: 'O'\n", i);
                 break;
 
             case 'u':
-            case 'U':
                 printf("%d: 'U'\n", i);
                 break;
 
             case 'y':
-            case 'Y':
                 if (i > 2) {
                     // its only sometimes Y
                     printf("%d: 'Y'\n", i);
+                    break;
                 }
-                break;
 
             default:
                 printf("%d: %c is not a vowel\n", i, letter);
